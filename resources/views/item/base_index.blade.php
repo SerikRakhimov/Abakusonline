8a196ae21dc515ee1a5c0de403c6518f81ce9d66
SerikRakhimov <s_astana@mail.ru>
2022-11-19 02:37:18 +0600

    Updated

--- resources/views/item/base_index.blade.php
+++ resources/views/item/base_index.blade.php
@@ -240,7 +240,7 @@
                             <p class="card-header text-center text-label">{{trans('main.code')}}: {{$item->code}}</p>
                         </a>
                     @endif
-                    <div class="card-body align-items-center">
+                    <div class="card-body align-items-stretch">
                         @if($item_find)
                             {{--                            <div class="card-block text-center">--}}
                             <div class="text-center">
