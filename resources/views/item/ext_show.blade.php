9d58f24ba93e98b2e88a608e85f9a2de29660986
SerikRakhimov <s_astana@mail.ru>
2023-01-06 12:54:01 +0600

    Updated

--- resources/views/item/ext_show.blade.php
+++ resources/views/item/ext_show.blade.php
@@ -164,14 +164,14 @@
                 <hr class="hr_ext_show">
                 <small class="text-label">
                     <small>
-                        {{--                            @if($base_link_right['is_bsmn_base_enable'] == true)--}}
-                        {{--                                <a href="{{route('item.base_index',['base'=>$link->parent_base_id, 'project'=>$project, 'role'=>$role, 'relit_id'=>$link->parent_relit_id])}}"--}}
-                        {{--                                   title="{{$link->parent_base->names($base_link_right)}}">--}}
-                        {{--                                    @include('layouts.item.ext_show.parent_label', ['link'=>$link, 'par_link'=>$par_link])--}}
-                        {{--                                </a>--}}
-                        {{--                            @else--}}
+                        @if($base_link_right['is_bsmn_base_enable'] == true)
+                            <a href="{{route('item.base_index',['base'=>$link->parent_base_id, 'project'=>$project, 'role'=>$role, 'relit_id'=>$link->parent_relit_id])}}"
+                               title="{{$link->parent_base->names($base_link_right)}}">
+                                @include('layouts.item.ext_show.parent_label', ['link'=>$link, 'par_link'=>$par_link])
+                            </a>
+                        @else
                         @include('layouts.item.ext_show.parent_label', ['link'=>$link, 'par_link'=>$par_link])
-                        {{--                            @endif--}}
+                                                    @endif
                     </small></small>
                 <div class="text-project">
                     @if($link->parent_base->type_is_text())
