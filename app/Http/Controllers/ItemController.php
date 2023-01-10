74cd7e3d227354b18c1f45c96b02d829cc21a17d
SerikRakhimov <s_astana@mail.ru>
2023-01-11 01:21:08 +0600

    Updated

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6377,7 +6377,7 @@ class ItemController extends Controller
             $item->name_lang_2 = $rs['calc_lang_2'];
             $item->name_lang_3 = $rs['calc_lang_3'];
             $item->save();
-            // Рекурсивный вызов для изменения вычисляемого наименования во вложенных записях
+            // Рекурсивный вызов для изменения вычисляемого наименования во вложенных записях, нужно
             self::calc_item_names($item);
         }
     }
