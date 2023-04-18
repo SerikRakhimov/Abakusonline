d174f8edae8075b111b58be4c294e6ae1897f750
SerikRakhimov <s_astana@mail.ru>
2023-04-18 09:52:00 +0600

    Updated

--- resources/views/item/ext_edit.blade.php
+++ resources/views/item/ext_edit.blade.php
@@ -1818,6 +1818,7 @@
 
                 {{--numcalc_{{$prefix}}{{$link->id}}.value = x;--}}
                 v.value = x;
+                alert(x);
             @endif
                 {{-- Похожие по смыслу проверки "$link->parent_is_nc_viewonly==false" в этом файле пять раз--}}
                 {{--            @if($link->parent_is_numcalc==true && $link->parent_is_nc_viewonly==false)--}}
