e5b626b8c14d85c808c1125be5dcd40b3513f1f0
SerikRakhimov <s_astana@mail.ru>
2023-05-12 00:27:41 +0600

    Updated

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -132,18 +132,17 @@ class Item extends Model
             } else {
                 $index = array_search(App::getLocale(), config('app.locales'));
                 if ($index !== false) {   // '!==' использовать, '!=' не использовать
-                    $result = "@@1".trim($this['name_lang_' . $index]);
+                    $result = trim($this['name_lang_' . $index]);
                     if ($fullname == true) {
                         //ограниченные 255 - размером полей хранятся в $item->name_lang_0 - $item->name_lang_3
                         $maxlen = 255;
-                        $result = "@@22aaa". $result;
                         if (($base->is_calcname_lst == true) && (mb_strlen($result) >= $maxlen)) {
                             // похожи GlobalController::itnm_left() и Item.php ("...")
-                            $result = "@@23bbb". mb_substr($result, $maxlen - 3, 3)."bbb".$result;
+                            $result = "bbb". mb_substr($result, $maxlen - 3, 3)."bbb".$result;
                             if (mb_substr($result, $maxlen - 3, 3) == "...") {
                                 // Полное наименование, более 255 символов
                                 //https://stackoverflow.com/questions/19693946/non-static-method-should-not-be-called-statically
-                                $result = "@@2".(new ItemController)->calc_value_func($this)['calc_full_lang_' . $index];
+                                $result = (new ItemController)->calc_value_func($this)['calc_full_lang_' . $index];
                             }
                         }
                     }
