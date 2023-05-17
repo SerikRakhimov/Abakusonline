a876843e3138cca5b3ab009876724c1ed7f9465f
SerikRakhimov <s_astana@mail.ru>
2023-05-17 19:41:36 +0600

    Updated

--- resources/views/list/table.blade.php
+++ resources/views/list/table.blade.php
@@ -258,7 +258,7 @@ $num_cols = GlobalController::get_number_of_columns_info();
             $i++;
             ?>
 {{--            <article style="text-indent: 40px; text-align: justify">--}}
-                            <article>
+                            <article style="text-align: justify">
                 {{--                <a href="{{route('item.ext_show', ['item'=>$item, 'project'=>$project, 'role'=>$role, 'usercode' =>GlobalController::usercode_calc(), 'relit_id'=>$relit_id,--}}
                 {{--                                                        'heading' => $heading,'base_index_page'=>$base_index_page, 'body_link_page'=>$body_link_page,'body_all_page'=>$body_all_page,--}}
                 {{--                                                        'par_link'=>null, 'parent_item'=>null,--}}
