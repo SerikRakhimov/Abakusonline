81604278c4502670dfe6b7db82f27e7c4d132d1a
SerikRakhimov <s_astana@mail.ru>
2023-07-25 14:33:18 +0600

    Updated

--- resources/views/layouts/project/show_relip_info.blade.php
+++ resources/views/layouts/project/show_relip_info.blade.php
@@ -3,5 +3,5 @@
         title="{{trans('main.project')}} ({{mb_strtolower(trans('main.name'))}})">{{$calc_relip_info['proj_name']}}</small>
 @endif
 @if($calc_relip_info['relit_title'] != '')
-    <h6 title="{{trans('main.relit')}} ({{mb_strtolower(trans('main.title'))}})">{{$calc_relip_info['relit_title']}}</h6>
+    <h5 title="{{trans('main.relit')}} ({{mb_strtolower(trans('main.title'))}})">{{$calc_relip_info['relit_title']}}</h5>
 @endif
