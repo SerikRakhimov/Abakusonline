f72df6a6d24d9577a4e08c1d6905383766ee95e3
SerikRakhimov <s_astana@mail.ru>
2023-08-08 15:12:38 +0600

    Updated

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1269,6 +1269,8 @@ class ProjectController extends Controller
             }
         }
 
+        // Нужна эта команда "Item::where('project_id', $project->id)->delete();"
+        Item::where('project_id', $project->id)->delete();
         $project->delete();
 
         if ($request->session()->has('projects_previous_url')) {
