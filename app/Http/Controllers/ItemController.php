0cf20bb1c666d03831bd853c2b4b98a8e03e5738
SerikRakhimov <s_astana@mail.ru>
2023-08-14 03:18:56 +0600

    Updated

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2995,7 +2995,7 @@ class ItemController extends Controller
 
         $links = $itpv->base->child_links()->get();
         foreach ($links as $key => $link) {
-            $inputs_reverse[$link->id] = -1;
+            //$inputs_reverse[$link->id] = -1;
             // см.комментарий выше
 //            if ($link->parent_base->type_is_image() | $link->parent_base->type_is_document()) {
 //                // "-1" - такое значение, чтобы не находилось Item::find() с таким значением
@@ -3003,9 +3003,9 @@ class ItemController extends Controller
 //            } elseif ($link->parent_is_base_link == true) {
 //                $inputs_reverse[$link->id] = $item->id;
 //            }
-            if ($link->parent_is_base_link == true) {
-                $inputs_reverse[$link->id] = $item->id;
-            }
+//            if ($link->parent_is_base_link == true) {
+//                $inputs_reverse[$link->id] = $item->id;
+//            }
         }
 
         $mains = $itpv->child_mains()->get();
