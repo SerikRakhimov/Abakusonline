b3fabfd91d1631c9f799d86c9d44d428e6c6b047
SerikRakhimov <s_astana@mail.ru>
2023-08-27 22:59:21 +0600

    Updated

--- resources/views/list/table.blade.php
+++ resources/views/list/table.blade.php
@@ -60,7 +60,7 @@ if ($view_link) {
                     {{--                <p class="card-header text-center text-label">{{$item->base->name()}}: {{$item->id}}--}}
                     <small class="card-header text-center text-title">
                         {{--                    {{$item->base->name()}} {{$item->id}}--}}
-                        {{$image_label}}
+                        {{$image_label}} {{$item->id}}
                         @if($base->is_code_needed == true)
                             {{--                        <a href="{{route('item.ext_show', ['item'=>$item, 'project'=>$project, 'role'=>$role, 'usercode' =>GlobalController::usercode_calc(), 'relit_id'=>$relit_id,--}}
                             {{--                                    'heading' => $heading,'base_index_page'=>$base_index_page, 'body_link_page'=>$body_link_page,'body_all_page'=>$body_all_page,--}}
