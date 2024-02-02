e6d85ab708ef70acc151c8651ec0d7d088f1bd94
SerikRakhimov <s_astana@mail.ru>
2024-02-03 04:38:46 +0600

    Updated

--- resources/views/item/ext_edit.blade.php
+++ resources/views/item/ext_edit.blade.php
@@ -1702,13 +1702,15 @@
                     ?>
 
                     {{--async - await нужно, https://tproger.ru/translations/understanding-async-await-in-javascript/--}}
-                    async function code_input_{{$prefix}}{{$link->id}}() {
+                    //111
+                    function code_input_{{$prefix}}{{$link->id}}() {
 alert('1');
                     {{--alert('1: '+'/item/item_from_base_code/'--}}
                     {{--    + '{{$link->parent_base_id}}'--}}
                     {{--    + '/' + '{{$relip_link_project->id}}'--}}
                     {{--    + '/' + code_{{$prefix}}{{$link->id}}.value);--}}
-                        await axios.get('/item/item_from_base_code/'
+                        //111
+                         axios.get('/item/item_from_base_code/'
                             + '{{$link->parent_base_id}}'
                             + '/' + '{{$relip_link_project->id}}'
                             + '/' + code_{{$prefix}}{{$link->id}}.value
