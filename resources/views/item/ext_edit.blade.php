980ee576ca009b99585aab08e55f9088fe5231ff
SerikRakhimov <s_astana@mail.ru>
2024-03-15 13:39:26 +0600

    Updated

--- resources/views/item/ext_edit.blade.php
+++ resources/views/item/ext_edit.blade.php
@@ -1801,7 +1801,8 @@
                 ?>
                 function link_id_change_{{$prefix}}{{$link->id}}() {
                     {{--alert('{{$link->id}}->>>' + child_base_id{{$prefix}}{{$link->id}}.value + 'child_code_id{{$link->id}}->>>' + child_code_id{{$prefix}}{{$link->id}}.value);--}}
-                    if (child_base_id{{$prefix}}{{$link->id}}.value == 0) {
+                        {{--if (child_base_id{{$prefix}}{{$link->id}}.value == 0) {--}}
+                    if (child_code_id{{$prefix}}{{$link->id}}.value == 0) {
                         parent_base_id{{$prefix}}{{$link->id}}.innerHTML = "{{trans('main.no_information') . '!'}}";
                         {{-- Такая проверка на '$link->parent_base->type_is_image()/!$link->parent_base->type_is_image()' в трех местах в этом файле--}}
                             @if(!$link->parent_base->type_is_image())
@@ -1831,9 +1832,9 @@
                                     {{-- "related_id" используется несколько раз по тексту --}}
                                     parent_related_id{{$prefix}}{{$link->id}}.innerHTML = res.data['result_item_id'];
                                 parent_unit_id{{$prefix}}{{$link->id}}.innerHTML = res.data['result_unit_name'];
-                                {{--alert('{{$link->id}}-> ' + child_base_id{{$prefix}}{{$link->id}}.value + ' 223-> ' + res.data['result_item_id'] + '-' + parent_related_id{{$prefix}}{{$link->id}}.innerHTML + ' ' + res.data['result_item_name'] + '-' + parent_base_id{{$prefix}}{{$link->id}}.innerHTML);--}}
+                                alert('{{$link->id}}-> ' + child_code_id{{$prefix}}{{$link->id}}.value + ' '+ child_base_id{{$prefix}}{{$link->id}}.value + ' 223-> ' + res.data['result_item_id'] + '-' + parent_related_id{{$prefix}}{{$link->id}}.innerHTML + ' ' + res.data['result_item_name'] + '-' + parent_base_id{{$prefix}}{{$link->id}}.innerHTML);
                                 @else
-                                {{--alert('{{$link->id}}-> ' + child_base_id{{$prefix}}{{$link->id}}.value + ' 223-> ' + res.data['result_item_name'] + '-' + parent_base_id{{$prefix}}{{$link->id}}.innerHTML);--}}
+                                alert('{{$link->id}}-> ' + child_code_id{{$prefix}}{{$link->id}}.value + ' ' + child_base_id{{$prefix}}{{$link->id}}.value + ' 223-> ' + res.data['result_item_name'] + '-' + parent_base_id{{$prefix}}{{$link->id}}.innerHTML);
                                 @endif
                                 {{-- Нужно использовать, см.примечание к on_parent_refer() ниже --}}
                                 {{-- Нужно, например, для правильного отображения данных при добавлении/корректировке записей в шаблоне "Интернет - магазин" --}}
