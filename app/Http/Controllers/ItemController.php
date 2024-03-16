78e0fa54963fe9e869a2f4cc4983c368102d7f44
SerikRakhimov <s_astana@mail.ru>
2024-03-16 16:57:17 +0600

    Updated

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6916,8 +6916,7 @@ class ItemController extends Controller
         $result_item_name = trans('main.no_information') . '!';
         $result_unit_name = "";
         $result_item_name_options = "";
-        //$item_start = Item::find($item_id);
-        $item_start = Item::find(22621);
+        $item_start = Item::find($item_id);
         if ($item_start) {
         // проверка, если link - вычисляемое поле
         if ($link_result->parent_is_parent_related == true) {
