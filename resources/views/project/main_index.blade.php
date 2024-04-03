95012224807dc356a40a364520bc6cb0d5689859
SerikRakhimov <s_astana@mail.ru>
2024-04-04 03:56:14 +0600

    Updated

--- resources/views/project/main_index.blade.php
+++ resources/views/project/main_index.blade.php
@@ -86,7 +86,7 @@
                         {{--                            <div class="card-block text-center">--}}
                         <div class="card-block text-center">
 {{--                        @include('view.img',['item'=>$get_project_logo_item, 'size'=>"medium", 'filenametrue'=>false, 'link'=>false, 'img_fluid'=>true, 'card_img_top'=>true, 'title'=>'empty'])--}}
-                            @include('view.img',['item'=>$get_project_logo_item, 'size'=>"medium", 'filenametrue'=>false, 'link'=>false, 'img_fluid'=>true, 'card_img_top'=>true, 'title'=>'empty'])
+                            @include('view.img',['item'=>$get_project_logo_item, 'filenametrue'=>false, 'link'=>false, 'img_fluid'=>true, 'card_img_top'=>true, 'title'=>'empty'])
                         </div>
                     @endif
                     <p>
