02273ab9d8a9faa4d99946d2490a3691757018b6
SerikRakhimov <s_astana@mail.ru>
2024-04-11 16:41:59 +0600

    Updated

--- resources/views/view/img.blade.php
+++ resources/views/view/img.blade.php
@@ -127,8 +127,8 @@ if ($item) {
                                     el.title = 'var1: ' + [tmp.real_width, tmp.real_height, window.innerHeight, window.innerWidth];
                             } else {
                                 {{--Одинаковый процент 0.75 layouts\app.php и view\img.php--}}
-                                    {{--    el.width = Math.int(window.innerWidth * {{$var_percent}} / 100 * 0.75);--}}
-                                    el.title = 'var2: ' + [tmp.real_width, tmp.real_height, window.innerHeight, window.innerWidth];
+                                        el.width = Math.int(window.innerWidth * {{$var_percent}} / 100 * 0.75);
+                                    el.title = 'var2: ' + [tmp.real_width, tmp.real_height, window.innerHeight, window.innerWidth, Math.int(window.innerWidth * {{$var_percent}} / 100 * 0.75];
                             }
                         }
                     </script>
