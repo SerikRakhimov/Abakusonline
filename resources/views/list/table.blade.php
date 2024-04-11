4e03d7ea31aab75139cbd468d758dcce5d507af2
SerikRakhimov <s_astana@mail.ru>
2024-04-11 13:52:13 +0600

    Updated

--- resources/views/list/table.blade.php
+++ resources/views/list/table.blade.php
@@ -243,14 +243,14 @@ if ($view_link) {
             ?>
             {{--            <article style="text-indent: 40px; text-align: justify">--}}
             <article style="text-align: justify">
-                {{--                <a href="{{route('item.ext_show', ['item'=>$item, 'project'=>$project, 'role'=>$role, 'usercode' =>GlobalController::usercode_calc(), 'relit_id'=>$relit_id,--}}
-                {{--                                                        'heading' => $heading,'base_index_page'=>$base_index_page, 'body_link_page'=>$body_link_page,'body_all_page'=>$body_all_page,--}}
-                {{--                                                        'par_link'=>null, 'parent_item'=>null,--}}
-                {{--                                                        'string_current' => $string_current,--}}
-                {{--                                                        ])}}"--}}
-                {{--                   title="{{trans('main.viewing_record')}}">--}}
-                {{--                    <span class="badge badge-related">{{$i}}</span>--}}
-                {{--                </a>--}}
+                <a href="{{route('item.ext_show', ['item'=>$item, 'project'=>$project, 'role'=>$role, 'usercode' =>GlobalController::usercode_calc(), 'relit_id'=>$relit_id,
+                                                                        'heading' => $heading,'base_index_page'=>$base_index_page, 'body_link_page'=>$body_link_page,'body_all_page'=>$body_all_page,
+                                                                        'par_link'=>null, 'parent_item'=>null,
+                                                                        'string_current' => $string_current,
+                                                                        ])}}"
+                   title="{{trans('main.viewing_record')}}">
+                    <span class="badge badge-related">{{$i}}</span>
+                </a>
                 <a href="{{route('item.item_index', ['project'=>$project, 'item'=>$item, 'role'=>$role,
         'usercode' =>GlobalController::usercode_calc(),
         'relit_id'=>$relit_id,
