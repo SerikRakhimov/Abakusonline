cb42ae5e0fff4e84682b2a15c8bdd41ae7325fcb
SerikRakhimov <s_astana@mail.ru>
2024-04-12 17:07:24 +0600

    Updated

--- resources/views/list/table.blade.php
+++ resources/views/list/table.blade.php
@@ -273,7 +273,7 @@ if ($view_link) {
                     @endif
                     {{-- 'Показывать дату создания'--}}
                     @if($base_right['is_list_base_sort_creation_date_desc'] == true)
-                        ({{$item->created_date()}})
+                            <i>({{$item->created_date()}})</i>
                     @endif
                 </a>
             </article>
