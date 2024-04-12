d5a14cbdc5a97bcb8c111c505f10d1df6174abd4
SerikRakhimov <s_astana@mail.ru>
2024-04-13 03:18:17 +0600

    Updated

--- resources/views/item/ext_show.blade.php
+++ resources/views/item/ext_show.blade.php
@@ -429,7 +429,7 @@
                                             {{--                                </b>--}}
                                         @elseif($link->parent_base->type_is_image())
                                             {{-- @include('view.img',['item'=>$item_find, 'size'=>"medium", 'border'=>true, 'filenametrue'=>false, 'link'=>true, 'img_fluid'=>false, 'card_img_top'=>false, 'title'=>""])--}}
-                                            @include('view.img',['item'=>$item_find, 'size'=>"small", 'border'=>false, 'filenametrue'=>false, 'link'=>true, 'img_fluid'=>false, 'card_img_top'=>false, 'title'=>""])
+                                            @include('view.img',['item'=>$item_find, 'size'=>"smed", 'border'=>false, 'filenametrue'=>false, 'link'=>true, 'img_fluid'=>false, 'card_img_top'=>false, 'title'=>""])
                                             {{--                            <a href="{{Storage::url($item_find->filename())}}">--}}
                                             {{--                                <img src="{{Storage::url($item_find->filename())}}" height="250"--}}
                                             {{--                                     alt="" title="{{$item_find->title_img()}}">--}}
