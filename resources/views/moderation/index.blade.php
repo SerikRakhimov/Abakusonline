baf778abfc42eef20977d6270f9ace695b991c93
SerikRakhimov <s_astana@mail.ru>
2025-01-20 18:45:01 +0600

    Updated

--- resources/views/moderation/index.blade.php
+++ resources/views/moderation/index.blade.php
@@ -58,7 +58,7 @@
                 <td class="text-left">
                     <a href="{{route('moderation.show',$item)}}" title="{{trans('main.show')}}">
                         {{$item->project->name()}}
-                        @include('layouts.project.show_icons',['project'=>$item->project])
+                        @include('layouts.project.show_logotype',['project'=>$item->project])
                     </a>
                 </td>
                 <td class="text-left">
