c5ee34c58963e2457434585012befad08cf97d6a
SerikRakhimov <s_astana@mail.ru>
2025-05-20 15:59:52 +0600

    Updated

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -3853,7 +3853,7 @@ class GlobalController extends Controller
                 $join->on('mains.child_item_id', '=', 'mains_dop_zs.child_item_id');
             })
             ->whereIn('mains_dop_zs.parent_item_id', $mains_sv)
-//            ->distinct()
+            ->distinct()
             ->groupBy('zv_id')
             ->groupBy('zs_id');
 
