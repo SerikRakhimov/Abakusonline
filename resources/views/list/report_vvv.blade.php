97d09dac2e0b2177a8d16b46e992fdede7f485e9
SerikRakhimov <s_astana@mail.ru>
2025-05-21 02:48:52 +0600

    Updated

--- resources/views/list/report_vvv.blade.php
+++ resources/views/list/report_vvv.blade.php
@@ -103,7 +103,8 @@
                             <summary></summary>
                             @foreach($zs_in_get as $d_value)
                                 <?php
-                                $item_sv = GlobalController::get_parent_item_from_main($d_value->zs_id, $link_body_id);
+                                //$item_sv = GlobalController::get_parent_item_from_main($d_value->zs_id, $link_body_id);
+                                $item_sv = Item::find($d_value->zs_id);
                                 ?>
                                 @if($item_sv)
                                     <?php
@@ -168,6 +169,7 @@
                                 @foreach($zs_notin_get as $d_value)
                                     <?php
                                     $item_sv = GlobalController::get_parent_item_from_main($d_value->zs_id, $link_body_id);
+                                    $item_sv = Item::find($d_value->zs_id);
                                     ?>
                                     @if($item_sv)
                                         <?php
