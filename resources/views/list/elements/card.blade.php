a29fa122ac9c0a50ceddc779342ceaed0f9c7b13
SerikRakhimov <s_astana@mail.ru>
2025-09-10 18:58:37 +0600

    Updated

--- resources/views/list/elements/card.blade.php
+++ resources/views/list/elements/card.blade.php
@@ -78,7 +78,7 @@ $my_bg_color = "#" . $hex_string;
                 {{--                echo $item->nmbr(true, false, false, false, false, GlobalController::set_un_all_par_link_null($view_link), true, true, $relit_id, $role);--}}
                 {{--                ?>--}}
 {{--                @include('layouts.item.name_with_image',['item'=>$item, 'size'=>"avatar", "circle"=>false])--}}
-                                <span style="color: {{$my_color}}; background-color: {{$my_bg_color}};">
+                                <span style="color: {{$my_bg_color}}; background-color: {{$my_color}};">
                 {{$item->name()}}
                             </span>
                 <small><i>{{GlobalController::calc_title_name($label_name,true,false)}}</i></small>
