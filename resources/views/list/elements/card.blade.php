710f918e422366a74b30a5f434627fe7a18e617a
SerikRakhimov <s_astana@mail.ru>
2025-09-10 18:54:12 +0600

    Updated

--- resources/views/list/elements/card.blade.php
+++ resources/views/list/elements/card.blade.php
@@ -2,9 +2,8 @@
 
 use \App\Http\Controllers\GlobalController;
 
-//$random_string = md5($item->id);
-//$hex_string = substr($random_string, 0, 6); // Получаем 10-значную шестнадцатеричную строку
-$hex_string = "a5f7g9";
+$random_string = md5($item->id);
+$hex_string = substr($random_string, 0, 6); // Получаем 10-значную шестнадцатеричную строку
 $my_color = "#FFFFFF";
 // Цвет фона
 $my_bg_color = "#" . $hex_string;
