4a861b8fdcfbd3ca3e27321757ee3692e2e96a6b
SerikRakhimov <s_astana@mail.ru>
2025-09-12 02:47:00 +0600

    Updated

--- resources/views/list/table.blade.php
+++ resources/views/list/table.blade.php
@@ -528,7 +528,7 @@ if ($v_link) {
                                                 @endif
                                                 {{-- Не удалять: предыдущий вариант--}}
                                                 {{-- nmbr(true): $fullname = true/false - вывод полной строки (более 255 символов), исключить $view_link при расчете вычисляемого наименования--}}
-                                                @include('layouts.item.empty_name', ['name'=>$item->nmbr(true, false, false, false, false, GlobalController::set_un_all_par_link_null($i_par_link), false, true, $relit_id, $role)])
+{{--                                                @include('layouts.item.empty_name', ['name'=>$item->nmbr(true, false, false, false, false, GlobalController::set_un_all_par_link_null($i_par_link), false, true, $relit_id, $role)])--}}
                                                 @include('layouts.item.name_with_image',['item'=>$item, 'size'=>"avatar", "circle"=>true])
                                                 @if ($item_index_view)
                                             </a>
