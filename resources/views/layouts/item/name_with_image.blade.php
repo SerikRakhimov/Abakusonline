f8ba4c436b0912449d7c76f7788834cab290cf83
SerikRakhimov <s_astana@mail.ru>
2025-09-25 12:38:41 +0600

    Updated

--- resources/views/layouts/item/name_with_image.blade.php
+++ resources/views/layouts/item/name_with_image.blade.php
@@ -21,38 +21,39 @@ if ($circle) {
 ?>
 <span class="d-flex flex-row align-items-stretch">
     <span class="pt-0">
-        {{-- Использовать "&&"--}}
-        @if($link_image && $item_find)
-            {{--        <span class="d-flex">--}}
-            {{--    @include('view.img',['item'=>$item_find, 'size'=>"avatar", 'filenametrue'=>false, 'border'=>true, 'link'=>false, 'img_fluid'=>false, 'card_img_top'=>false, 'title'=>trans('main.author')])--}}
-            @include('view.img',['item'=>$item_find, 'size'=>$size, 'circle'=>$circ_para, 'noimg_def'=>true, 'filenametrue'=>false, 'link'=>false, 'img_fluid'=>false, 'card_img_top'=>false, 'title'=>trans('main.author')])
-            {{--                    </span>--}}
-        @else
-            {{--        <span class="d-flex flex-nowrap">--}}
-            {{-- Вывод буквы в кружочке--}}
-            <?php
-            $item_color = $item;
-            // Выводить связанное поле
-            if ($link_image->parent_is_parent_related == true) {
-                $item_color = GlobalController::view_info($item->id, $link_image->parent_parent_related_start_link_id, $role, $relit_id, true);
-            }
-            // Проверка с командой внутри нужна
-            if (!$item_color) {
+        @if($link_image)
+            @if($item_find)
+                {{--        <span class="d-flex">--}}
+                {{--    @include('view.img',['item'=>$item_find, 'size'=>"avatar", 'filenametrue'=>false, 'border'=>true, 'link'=>false, 'img_fluid'=>false, 'card_img_top'=>false, 'title'=>trans('main.author')])--}}
+                @include('view.img',['item'=>$item_find, 'size'=>$size, 'circle'=>$circ_para, 'noimg_def'=>true, 'filenametrue'=>false, 'link'=>false, 'img_fluid'=>false, 'card_img_top'=>false, 'title'=>trans('main.author')])
+                {{--                    </span>--}}
+            @else
+                {{--        <span class="d-flex flex-nowrap">--}}
+                {{-- Вывод буквы в кружочке--}}
+                <?php
                 $item_color = $item;
-            }
-            $height = GlobalController::types_img_height($size, false);
-            $h_text = $height;
-            // $rnd_colors = GlobalController::rnd_colors($item->id);
-            $rnd_colors = GlobalController::rnd_colors($item_color->id);
-            // Цвет текста: "#FFFFFF" - белый цвет
-            $my_color = $rnd_colors['my_color'];
-            // Цвет фона
-            $my_bg_color = $rnd_colors['my_bg_color'];
-            ?>
-            <span class="{{$circle_rounded}} d-flex align-items-center justify-content-center font-weight-bold"
-                  style="width: {{$h_text}}px; height: {{$h_text}}px; color: {{$my_color}}; background-color: {{$my_bg_color}}; float: left; margin: 3px;">
+                // Выводить связанное поле
+                if ($link_image->parent_is_parent_related == true) {
+                    $item_color = GlobalController::view_info($item->id, $link_image->parent_parent_related_start_link_id, $role, $relit_id, true);
+                }
+                // Проверка с командой внутри нужна
+                if (!$item_color) {
+                    $item_color = $item;
+                }
+                $height = GlobalController::types_img_height($size, false);
+                $h_text = $height;
+                // $rnd_colors = GlobalController::rnd_colors($item->id);
+                $rnd_colors = GlobalController::rnd_colors($item_color->id);
+                // Цвет текста: "#FFFFFF" - белый цвет
+                $my_color = $rnd_colors['my_color'];
+                // Цвет фона
+                $my_bg_color = $rnd_colors['my_bg_color'];
+                ?>
+                <span class="{{$circle_rounded}} d-flex align-items-center justify-content-center font-weight-bold"
+                      style="width: {{$h_text}}px; height: {{$h_text}}px; color: {{$my_color}}; background-color: {{$my_bg_color}}; float: left; margin: 3px;">
                                 {{mb_strtoupper(mb_substr($it_nm, 0, 1))}}
             </span>
+            @endif
         @endif
         {{$relit_id}}
     </span>
